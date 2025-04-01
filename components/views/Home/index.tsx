56c9f7867920361fc425e278f9e80ebbbabaf87e
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-01 17:40:58 +0900

    add : shadcn ui 추가 및 테스트

---
+++ components/views/Home/index.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { Button } from "@/components/ui/button";
+
+const HomeView = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <Button className="cursor-pointer">Click me</Button>
+        </div>
+    );
+};
+
+export default HomeView;
