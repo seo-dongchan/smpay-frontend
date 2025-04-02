a7dfc2497601a68241e5d26591374cdf0e9970de
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-02 13:41:52 +0900

    fix : 불필요 코드 정리

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,12 +1,13 @@
+import Link from 'next/link';
 import { Separator } from '@/components/ui/separator';
 import UserMenu from './UserMenu';
 
 const Header = () => {
   return (
     <header className="h-full flex justify-end items-center space-x-4 text-sm py-3 px-4">
-      <div>Blog</div>
+      <Link href="/support">고객센터</Link>
       <Separator orientation="vertical" />
-      <div>Docs</div>
+      <Link href="/notice">공지사항</Link>
       <Separator orientation="vertical" />
       <UserMenu />
     </header>
