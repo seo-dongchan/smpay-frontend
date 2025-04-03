edcd201539a946c319ae8b21944454a2ebe186cc
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-03 18:24:23 +0900

    fix : 사이드바 해당 위치 표시 수정(미완)

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -654,11 +654,15 @@ function SidebarMenuSubButton({
       data-size={size}
       data-active={isActive}
       className={cn(
-        'text-sidebar-foreground ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground [&>svg]:text-sidebar-accent-foreground flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 outline-hidden focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
-        'data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground',
+        'flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 outline-hidden focus-visible:ring-2',
+        'text-sm hover:bg-sidebar-accent hover:text-sidebar-accent-foreground',
+        'disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50',
+        '[&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0',
+        'group-data-[collapsible=icon]:hidden',
         size === 'sm' && 'text-xs',
         size === 'md' && 'text-sm',
-        'group-data-[collapsible=icon]:hidden',
+
+        isActive && 'bg-[#e4f0ff] text-[#1e40af] font-semibold shadow-inner ring-1 ring-[#60a5fa]',
         className,
       )}
       {...props}
