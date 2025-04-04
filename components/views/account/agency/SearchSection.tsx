a75f60ebdf5e189a60e913add29c1bc420fb3116
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-04 17:13:37 +0900

    add : 대행사 관리 > top 구역 생성 (ui)만

---
+++ components/views/account/agency/SearchSection.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@/components/ui/button';
+import { SearchInput } from '@/components/ui/input';
+
+const SearchSection = () => {
+  return (
+    <section className="mt-4 bg-[#F2F2F2] h-[65px] flex items-center justify-between p-4">
+      <div className="flex items-center gap-2">
+        <SearchInput className="w-[425px]" />
+        <Button>검색</Button>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <LinkButton>대행사 등록</LinkButton>
+        <LinkButton>회원 등록</LinkButton>
+      </div>
+    </section>
+  );
+};
+
+export default SearchSection;
+
+const LinkButton = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+  return (
+    <Button
+      variant="ghost"
+      className="w-[125px] bg-[#F6BE2C] hover:bg-[#F6BE2C] text-black font-bold"
+    >
+      {children}
+    </Button>
+  );
+};
