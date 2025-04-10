7f2214570e7f1d8e6edcb39d9f51699bebf123cf
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-10 11:01:31 +0900

    fix : 테마 색상 예시 변경(버튼)

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-[#545F71] text-white shadow-xs hover:bg-[#434d5d]',
+        default: 'bg-primary text-white shadow-xs hover:bg-primary/90',
+        // default: 'bg-[#545F71] text-white shadow-xs hover:bg-[#434d5d]',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
