89da676ffa62501f333b2570457295643a5d47e1
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-11 16:18:58 +0900

    add : 계정 관리 > 회원등록  > 초대 메일 발송 화면 개발

---
+++ app/account/member-management/page.tsx
@@ -0,0 +1,27 @@
+import ContentHeader from '@/components/common/ContentHeader';
+import MemberManagementView from '@/components/views/account/member-management';
+import { DashboardSubItem } from '@/constants/dasboard';
+
+export default function MemberManagementPage() {
+  return (
+    <div>
+      <ContentHeader title="회원 관리" items={breadcrumbItems} />
+      <MemberManagementView />
+    </div>
+  );
+}
+
+const breadcrumbItems: DashboardSubItem[] = [
+  {
+    title: '계정 관리',
+    url: '/account',
+  },
+  {
+    title: '회원 관리',
+    url: '/account/member-management',
+  },
+  {
+    title: '회원 등록',
+    url: '#',
+  },
+];
