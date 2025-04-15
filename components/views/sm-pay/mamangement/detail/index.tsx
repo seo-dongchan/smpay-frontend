75be20d782f9ab1fc963c9f4b0ddd23f48598e4c
seo-dongchan <dongchan.seo@searchm.co.kr>
2025-04-15 11:55:26 +0900

    etc : 페이지 추후 수정 관련 내용 정리 주석 추가

--- components/views/sm-pay/mamangement/detail/index.tsx
+++ components/views/sm-pay/mamangement/detail/index.tsx
@@ -5,6 +5,8 @@ import RuleSection from './RuleSection';
 import ScheduleSection from './ScheduleSection';
 import GuidSection from '@/components/common/sm-pay/GuideSection';
 
+// [대행사] SM Pay 관리 -> 조회 -> 신청 내역 상세
+// TODO : agency/sm-pay/management/[slug]  변경
 const SmPayManagementDetailView = () => {
   return (
     <div>
